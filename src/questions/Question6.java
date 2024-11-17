7184b5ee9073353c90b5ffa43fe2ed65c7f5c169
AjafaM <149159094+AjaanulfaM@users.noreply.github.com>
2024-11-17 12:56:17 +0000

    Completed Q6
    1.) Moved methods validateRange and displayFlights to UtilityClass.
    2.) Handled cases for empty queues if trying to display them

--- src/questions/Question6.java
+++ src/questions/Question6.java
@@ -16,38 +16,18 @@ public class Question6 { // Flight take-off (Queue)
     static Queue<String> forLanding = new ArrayDeque<String>();
 
     public static void main(String[] args) {
-//        An airport has only one runway. When it is busy, planes wishing to take off or land have to
-//        wait. Implement a simulation, using two queues, one each for the planes waiting to take off
-//        and land. Landing planes get priority.
-//        The user enters commands:
-//                > takeoff flightCode
-//                > land flightCode
-//                > next
-//                > quit
-
-//        The first two commands place the flight in the appropriate queue. The next command
-//        finishes the current take-off or landing and enables the next one, printing the action (take
-//        off or land) and the flight symbol.
-//        For example:
-    //        takeoff( “Flight-100”); // is queued for ‘take off’
-    //        takeoff(“Flight-220”); // is queued
-    //        land(“Flight-320”); // is queued for landing
-    //        takeoff(“Flight-EI104”); // queued for takeoff
-    //        next(); // will complete landing of the landing of Flight-320
-    //        next(); // will complete takeoff of Flight-100
-
+        System.out.println("FLIGHT TAKE-OFF SIMULATOR");
         menuOptions();
     }
 
     public static void menuOptions() {
-        System.out.println("\nFLIGHT TAKE-OFF SIMULATOR\n" +
-                "Choose one of the options (1-4)\n" +
+        System.out.println("Choose one of the options (1-5)\n" +
                 "1. Queue takeoff\n" +
                 "2. Queue landing\n" +
-                "3. Queue next flight\n" +
+                "3. Send flight for take-off\n" +
                 "4. View flights\n" +
                 "5. End application");
-        int choice = validateRange(1, 5);
+        int choice = UtilityClass.validateRange(1, 5);
 
         menu(choice);
     }
@@ -67,7 +47,7 @@ public class Question6 { // Flight take-off (Queue)
                 }
             }
 
-            System.out.println("Ending...");
+            System.out.println("Ending...\n");
             menuOptions();
         }
         else if(choice == 2) {
@@ -82,19 +62,39 @@ public class Question6 { // Flight take-off (Queue)
                 }
             }
 
-            System.out.println("Ending...");
-            
+            System.out.println("Ending...\n");
+
             menuOptions();
         }
         else if(choice == 3) {
-            System.out.println("Flight 1 has now landed. Flight 2 is now taking off. Flight 3 is preparing for takeoff:");
+            if(!forTakeoff.isEmpty()) {
+                String takeoffFLight = forTakeoff.remove();
+                System.out.println(takeoffFLight+ " is now taking off.");
+            }
+            else {
+                System.out.println("No flights queued for take off");
+            }
+
+            if(!forLanding.isEmpty()) {
+                String landingFlight = forLanding.remove();
+                System.out.println(landingFlight+ " is now landing.\n");
+
+                forTakeoff.add(landingFlight);
+            }
+            else {
+                System.out.println("No flights queued for landing\n");
+            }
+
+            menuOptions();
         }
         else if(choice == 4) {
             System.out.println("Flights queued for landing:");
-            displayFlights(forLanding);
+            UtilityClass.displayQueueString(forLanding);
 
-            System.out.println("Flights queued for take-off");
-            displayFlights(forTakeoff);
+            System.out.println("Flights queued for take-off:");
+            UtilityClass.displayQueueString(forTakeoff);
+
+            System.out.print("\n");
 
             menuOptions();
         }
@@ -123,49 +123,5 @@ public class Question6 { // Flight take-off (Queue)
         }
 
         return code;
-    }
-
-    public static void displayFlights(Queue<String> queue) {
-        int counter = 0;
-
-        if(!queue.isEmpty()) {
-            System.out.print("[");
-            for (String flight : queue) {
-                counter++;
-
-                if (counter != queue.size()) {
-                    System.out.print(flight + ", ");
-                } else {
-                    System.out.println(flight + "]");
-                }
-            }
-        }
-        else {
-            System.out.println("There are currently no flights");
-        }
-    }
-
-    public static int validateRange(int min, int max) {
-        int input = 0;
-        boolean done = false;
-
-        while(!done) {
-            if(scanner.hasNextInt()) {
-                input = scanner.nextInt();
-
-                if(input < min || input > max) {
-                    System.out.println("Please enter a valid option (1-2)");
-                }
-                else {
-                    done = true;
-                }
-            }
-            else {
-                System.out.println("Please enter only integer values");
-                scanner.next();
-            }
-        }
-
-        return input;
     }
 }
