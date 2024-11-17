f21a6fc578e5612536f19b86919de8e603e4d914
AjafaM <149159094+AjaanulfaM@users.noreply.github.com>
2024-11-17 06:26:10 +0000

    Initial commit

---
+++ src/questions/Question6.java
@@ -0,0 +1,12 @@
+package questions;
+
+/**
+ *  Name: Theo Picar
+ *  Class Group: SD2A
+ */
+
+public class Question6 { // Flight take-off (Queue)
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+    }
+}
