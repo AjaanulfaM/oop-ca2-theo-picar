f28f67567e8029fd286a2eddae6e48f0753f1659
AjafaM <149159094+AjaanulfaM@users.noreply.github.com>
2024-11-17 06:29:02 +0000

    Just testing out GitHub

--- src/questions/Question6.java
+++ src/questions/Question6.java
@@ -8,5 +8,7 @@ package questions;
 public class Question6 { // Flight take-off (Queue)
     public static void main(String[] args) {
         System.out.println("Testing...");
+        System.out.println("Testing again...");
+        System.out.println("Hello???");
     }
 }
