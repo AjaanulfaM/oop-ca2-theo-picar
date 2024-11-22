0451ecbf3c525dbe30b97aaa357515121ae16bad
AjafaM <149159094+AjaanulfaM@users.noreply.github.com>
2024-11-22 10:51:13 +0000

    Removed buy and sell properties

--- src/questions/question8/Block.java
+++ src/questions/question8/Block.java
@@ -1,23 +1,15 @@
 package questions.question8;
 
 public class Block {
-    private double buy;
     private double quantity;
     private double price;
-    private String symbol;
 
-    public Block(double buy, double quantity, double price, String symbol) {
-        this.buy = buy;
+    public Block(double quantity, double price) {
         this.quantity = quantity;
         this.price = price;
-        this.symbol = symbol;
     }
 
     // Getters
-    public double getBuy() {
-        return this.buy;
-    }
-
     public double getQuantity() {
         return this.quantity;
     }
@@ -26,25 +18,13 @@ public class Block {
         return this.price;
     }
 
-    public String getSymbol() {
-        return this.symbol;
-    }
-
     // Setters
-    public void setBuy(double buy) {
-        this.buy = buy;
-    }
-
     public void setQuantity(double quantity) {
         this.quantity = quantity;
     }
 
     public void setPrice(double quantity) {
         this.quantity = quantity;
-    }
-
-    public void setSymbol(String quantity) {
-        this.symbol = quantity;
     }
 }
 
