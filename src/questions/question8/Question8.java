09f3a78a43a3126a15ffde89825ef50eedaa96ee
AjafaM <149159094+AjaanulfaM@users.noreply.github.com>
2024-11-22 09:36:40 +0000

    Reduced number of options
    1.) Only added user commands 'buy' and 'sell'

--- src/questions/question8/Question8.java
+++ src/questions/question8/Question8.java
@@ -9,7 +9,7 @@ import java.util.Queue;
 
 public class Question8 {
     static Queue<Block> stock = new ArrayDeque<>();
-    static Map<String, Queue<Block>> map = new HashMap<>();
+    static Map<String, Queue<Block>> companyStocks = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("STOCK SIMULATOR");
@@ -17,17 +17,15 @@ public class Question8 {
     }
 
     public static void menuOptions() {
-        String[] options = new String[5];
+        String[] options = new String[3];
         options[0] = "Buy stock";
         options[1] = "Sell stock";
-        options[2] = "Stock symbol";
-        options[3] = "Stock price";
-        options[4] = "Quit application";
+        options[2] = "Quit application";
 
-        System.out.println("Please choose one of the following (1-5)");
+        System.out.println("Please choose one of the following (1-3)");
         UtilityClass.menuOptions(options);
 
-        int choice = UtilityClass.validateRange(1, 5);
+        int choice = UtilityClass.validateRange(1, 3);
 
         menu(choice);
     }
@@ -38,12 +36,6 @@ public class Question8 {
         }
         else if(choice == 2) {
             System.out.println("Sell stock");
-        }
-        else if(choice == 3) {
-            System.out.println("Stock symbol");
-        }
-        else if(choice == 4) {
-            System.out.println("Stock price");
         }
         else {
             System.out.println("Ending session...\nDone. Goodbye");
