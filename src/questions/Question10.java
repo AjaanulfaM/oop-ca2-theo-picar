d7470ec29cf3a583a414323a65ea3671c755ee3b
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-03 18:17:03 +0000

    See description
    1. Decided to use maze in given example on PDF
    2. Fully changed menu. User can either view maze, quit, or start algorithm

--- src/questions/Question10.java
+++ src/questions/Question10.java
@@ -8,81 +8,60 @@ public class Question10 {
 
     public static void menuOptions() {
         String[] options = {
-                "Maze 1 - Easy",
-                "Maze 2 - Medium",
-                "Maze 3 - Hard",
+                "Start Navigating",
+                "View Maze",
                 "Quit application",
         };
         UtilityClass.menuOptions(options);
 
-        System.out.println("Choose one of the above options (1-4)");
-        int choice = UtilityClass.validateRange(1, 4);
+        System.out.println("Choose one of the above options (1-3)");
+        int choice = UtilityClass.validateRange(1, 3);
 
-        if(choice == 4) {
-            System.out.println("Ending session...\nDone! Goodbye.");
+        if(choice == 1) {
+            mazeMenu();
+        }
+        else if(choice == 2){
+            int[][] maze = createMaze();
+
+            for(int i = 0; i < maze.length; i++) {
+                System.out.println();
+
+                for(int j = 0; j < maze.length; j++) {
+                    // wall
+                    if(maze[i][j] == 0) {
+                        System.out.print("[]");
+                    }
+                    // path (two spaces so it formats correctly on terminal)
+                    else {
+                        System.out.print("  ");
+                    }
+                }
+            }
+
+            System.out.println("\n");
+            menuOptions();
         }
         else {
-            mazeMenu(choice);
+            System.out.println("Ending session...\nDone! Goodbye.");
         }
     }
 
-    public static void mazeMenu(int level) {
-        int[][] maze = createMaze(level);
-
-        for(int i = 0; i < maze.length; i++) {
-            System.out.println();
-
-            for(int j = 0; j < maze.length; j++) {
-                // wall
-                if(maze[i][j] == 0) {
-                    System.out.print("[]");
-                }
-                // path (two spaces so it formats correctly on terminal)
-                else {
-                    System.out.print("  ");
-                }
-            }
-        }
-
+    public static void mazeMenu() {
         System.out.println("\n");
         menuOptions();
     }
 
-    public static int[][] createMaze(int level) {
-        if(level == 1) {
-            return new int[][] {
-                    {0, 0, 0, 0, 0},
-                    {1, 1, 1, 0, 0},
-                    {0, 0, 1, 0, 0},
-                    {0, 0, 1, 0, 0},
-                    {0, 0, 0, 0, 0}
-            };
-        }
-        else if(level == 2) {
-            return new int[][] {
-                    {0, 0, 1, 0, 0, 0, 0},
-                    {0, 0, 1, 0, 0, 0, 0},
-                    {0, 1, 1, 1, 0, 1, 0},
-                    {0, 1, 0, 0, 0, 1, 0},
-                    {0, 1, 0, 1, 0, 1, 0},
-                    {0, 1, 1, 1, 1, 1, 0},
-                    {0, 0, 0, 0, 0, 0, 0},
-            };
-        }
-        else {
-            return new int[][] {
-                    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-                    {0, 1, 1, 1, 1, 1, 0, 1, 0, 0},
-                    {0, 0, 1, 0, 0, 1, 0, 1, 1, 0},
-                    {0, 0, 1, 0, 1, 1, 0, 0, 1, 0},
-                    {0, 0, 0, 1, 0, 1, 1, 0, 1, 0},
-                    {0, 0, 1, 1, 1, 1, 0, 0, 1, 0},
-                    {0, 0, 1, 0, 0, 1, 0, 1, 1, 0},
-                    {0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
-                    {0, 0, 1, 1, 1, 1, 1, 1, 0, 0},
-                    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-            };
-        }
+    public static int[][] createMaze() {
+        return new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {1, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 1, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
     }
 
 }
