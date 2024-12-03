b3ff0d857108570a646ffd4b8a6678a439a95ea6
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-03 19:20:29 +0000

    Implementing basics of backtracking

--- src/questions/Question10.java
+++ src/questions/Question10.java
@@ -1,5 +1,8 @@
 package questions;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Question10 {
     public static void main(String[] args) {
         System.out.println("BACKTRACKING ALGORITHM");
@@ -21,22 +24,7 @@ public class Question10 {
             mazeMenu();
         }
         else if(choice == 2){
-            int[][] maze = createMaze();
-
-            for(int i = 0; i < maze.length; i++) {
-                System.out.println();
-
-                for(int j = 0; j < maze.length; j++) {
-                    // wall
-                    if(maze[i][j] == 0) {
-                        System.out.print("[]");
-                    }
-                    // path (two spaces so it formats correctly on terminal)
-                    else {
-                        System.out.print("  ");
-                    }
-                }
-            }
+            outputMaze();
 
             System.out.println("\n");
             menuOptions();
@@ -47,6 +35,38 @@ public class Question10 {
     }
 
     public static void mazeMenu() {
+        Deque<int[]> position = new ArrayDeque<>();
+        Deque<String> direction = new ArrayDeque<>();
+        int row = 3;
+        int column = 4;
+
+        // Manually give starting position in maze and push it to stack
+        position.push(new int[]{row, column});
+
+        // Push all possible paths from the starting position
+        String N = "up", E = "right", S = "down", W = "left";
+        direction.push(N);
+        direction.push(E);
+        direction.push(S);
+        direction.push(W);
+
+        boolean exitFound = false;
+        while(!exitFound) {
+            if(direction.pop().equals("up")) {
+                row--;
+            }
+            else if(direction.pop().equals("right")) {
+                column++;
+            }
+            else if(direction.pop().equals("down")) {
+                row++;
+            }
+            else {
+                column--;
+            }
+        }
+
+
         System.out.println("\n");
         menuOptions();
     }
@@ -62,6 +82,27 @@ public class Question10 {
                 {0, 0, 0, 0, 1, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0}
         };
+    }
+
+    public static void outputMaze() {
+        int[][] maze = createMaze();
+
+        for(int i = 0; i < maze.length; i++) {
+            if(i != 0)  {
+                System.out.println();
+            }
+
+            for(int j = 0; j < maze.length; j++) {
+                // wall
+                if(maze[i][j] == 0) {
+                    System.out.print("[]");
+                }
+                // path (two spaces so it formats correctly on terminal)
+                else {
+                    System.out.print("  ");
+                }
+            }
+        }
     }
 
 }
