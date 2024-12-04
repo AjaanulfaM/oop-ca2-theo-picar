615148af0bb640a52f859f6a636b76f39b05bf1a
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-04 12:13:50 +0000

    Re-doing navigation algorithm
    1. Working on one direction first to avoid getting overwhelmed
    2. Changed the way I am navigating through maze
    3. Changed some variable names

--- src/questions/Question10.java
+++ src/questions/Question10.java
@@ -35,98 +35,52 @@ public class Question10 {
     }
 
     public static void mazeMenu() {
-        Deque<int[]> position = new ArrayDeque<>();
-        Deque<String> direction = new ArrayDeque<>();
+        Deque<int[]> positions = new ArrayDeque<>();
+        Deque<String> directions = new ArrayDeque<>();
         int row = 3, column = 4;
         int[][] mazePosition = createMaze();
-
-        // Manually give starting position in maze and push it to stack
-        position.push(new int[]{row, column});
-
-        // Push all possible paths from the starting position. (North, East, South, West)
         String N = "up", E = "right", S = "down", W = "left";
-        direction.push(N);
-        direction.push(E);
-        direction.push(S);
-        direction.push(W);
+
+        // Push all possible paths from the starting position [3][4]
+        positions.push(new int[]{row, column});
+        directions.push(W);
+
+        positions.push(new int[]{row, column});
+        directions.push(S);
+
+        positions.push(new int[]{row, column});
+        directions.push(E);
+
+        positions.push(new int[]{row, column});
+        directions.push(N);
 
         boolean exitFound = false;
         while(!exitFound) {
+            int[] newRowAndColumn = positions.pop();
+            String directionToMove = directions.pop();
+
+            row = newRowAndColumn[0];
+            column = newRowAndColumn[1];
+
             boolean done = false;
-
-            if(direction.pop().equals("up")) {
+            if (directionToMove.equals("up")) {
                 while(!done) {
-                    row--;
-
-                    if(mazePosition[row][column] == 0) {
+                    // move up until we hit a wall / dead end
+                    if(mazePosition[row-1][column] == 0) {
                         done = true;
                     }
-                    // check if there is a left path on current position
-                    if(mazePosition[row][column - 1] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(W);
+                    else {
+                        row--;
                     }
-                    // check if there is a right path on current position
-                    if(mazePosition[row][column + 1] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(E);
-                    }
-                }
-            }
-            else if(direction.pop().equals("right")) {
-                while(!done) {
-                    column++;
 
-                    if(mazePosition[row][column] == 0) {
-                        done = true;
+                    // as we're moving along, check if there are any paths along the way
+                    if(mazePosition[row][column-1] == 1) {
+                        positions.push(new int[] {row, column});
+                        directions.push(W);
                     }
-                    // check if there is an up path on current position
-                    if(mazePosition[row - 1][column] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(N);
-                    }
-                    // check if there is a down path on current position
-                    if(mazePosition[row + 1][column] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(E);
-                    }
-                }
-            }
-            else if(direction.pop().equals("down")) {
-                while(!done) {
-                    row++;
-
-                    if(mazePosition[row][column] == 0) {
-                        done = true;
-                    }
-                    // check if there is a left path on current position
-                    if(mazePosition[row][column - 1] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(W);
-                    }
-                    // check if there is a right path on current position
-                    if(mazePosition[row][column + 1] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(E);
-                    }
-                }
-            }
-            else {
-                while(!done) {
-                    column--;
-
-                    if(mazePosition[row][column] == 0) {
-                        done = true;
-                    }
-                    // check if there is an up path on current position
-                    if(mazePosition[row - 1][column] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(N);
-                    }
-                    // check if there is a down path on current position
-                    if(mazePosition[row + 1][column] == 1) {
-                        position.push(new int[]{row, column});
-                        direction.push(E);
+                    if(mazePosition[row][column+1] == 1) {
+                        positions.push(new int[] {row, column});
+                        directions.push(E);
                     }
                 }
             }
@@ -136,6 +90,10 @@ public class Question10 {
         System.out.println("\n");
         menuOptions();
     }
+
+//    public static void checkValidPaths(String direction, int row, int column, int[][] mazePosition) {
+//
+//    }
 
     public static int[][] createMaze() {
         return new int[][] {
@@ -162,6 +120,9 @@ public class Question10 {
                 // wall
                 if(maze[i][j] == 0) {
                     System.out.print("[]");
+                }
+                else if(i == 3 && j == 4) {
+                    System.out.print("<>");
                 }
                 // path (two spaces so it formats correctly on terminal)
                 else {
