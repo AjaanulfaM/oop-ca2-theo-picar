fa546ab130c2e9e24b262892d7865bb648f196f5
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2024-12-07 07:16:40 +0000

    Implemented final version from group repo

--- src/questions/Question10.java
+++ src/questions/Question10.java
@@ -1,16 +1,20 @@
 package questions;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
-// Bug in code: If an intersection happens to be at end of path, the paths will be checked twice at that intersection
-// Plus have to manually give starting directions and position otherwise code breaks
+// Bug for mazes with loops: Some paths may be checked twice
 
 public class Question10 {
 
     static String N = "up", E = "right", S = "down", W = "left";
+
     static boolean exitFound = false;
     static int attempts = 0;
+
+    static Deque<int[]> positions = new ArrayDeque<>();
+    static Deque<String> directions = new ArrayDeque<>();
+    // to avoid pushing paths twice from an intersection that happens to be at a dead end
+    static ArrayList<String> visitedIntersections = new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("BACKTRACKING ALGORITHM");
@@ -22,6 +26,7 @@ public class Question10 {
                 "Start Navigating",
                 "Quit application"
         };
+
         UtilityClass.menuOptions(options);
 
         System.out.println("Choose one of the above options (1-2)");
@@ -36,51 +41,65 @@ public class Question10 {
     }
 
     public static void mazeMenu() {
-        Deque<int[]> positions = new ArrayDeque<>();
-        Deque<String> directions = new ArrayDeque<>();
-        int row = 3, column = 4;
-        int[][] mazePosition = createMaze();
+        int[][] mazePosition = createPDFMaze();
+        int row = 0, column = 0;
 
-        // Push all possible paths from the starting position [3][4]
-        positions.push(new int[]{row, column});
-        directions.push(W);
+        boolean done = false;
+        while(!done) {
+            System.out.println("Please enter the row you would like to start at (0-7)");
+            row = UtilityClass.validateRange(0, 7);
+
+            System.out.println("Please enter the column you would like to start at");
+            column = UtilityClass.validateRange(0, 7);
+
+            if(mazePosition[row][column] == 1) {
+                System.out.println("You are starting at position [" +row+ ", " +column+ "]. Navigating the maze...");
+                done = true;
+            }
+            else {
+                System.out.println("Invalid position! You must choose a position on a path.");
+            }
+        }
 
         positions.push(new int[]{row, column});
-        directions.push(E);
 
-        positions.push(new int[]{row, column});
-        directions.push(N);
-
-        positions.push(new int[]{row, column});
-        directions.push(S);
+        // checking for all possible paths from the user's starting position
+        checkPathsAtIntersection(mazePosition, "left&right", row, column);
+        checkPathsAtIntersection(mazePosition, "up&down", row, column);
 
         while(!exitFound) {
             // re-assign variables every time a dead end is found
-            int[] newRowAndColumn = positions.pop();
-            String directionToMove = directions.pop();
+            int[] newRowAndColumn = new int[2];
+            String directionToMove = "";
+
+            if(!positions.isEmpty() && !directions.isEmpty()) {
+                newRowAndColumn = positions.pop();
+                directionToMove = directions.pop();
+            }
+            else {
+                System.out.println("There are no exits in this maze!\n");
+                menuOptions();
+            }
 
             row = newRowAndColumn[0];
             column = newRowAndColumn[1];
 
             if(directionToMove.equals("left") || directionToMove.equals("right")) {
-                handleLeftRightMovement(directionToMove, row, column, mazePosition, positions, directions);
+                handleLeftRightMovement(directionToMove, row, column, mazePosition);
             }
             else {
-                handleUpDownMovement(directionToMove, row, column, mazePosition, positions, directions);
+                handleUpDownMovement(directionToMove, row, column, mazePosition);
             }
         }
 
         menuOptions();
     }
 
-    public static void handleLeftRightMovement(String directionToMove, int row, int column, int[][] mazePosition, Deque<int[]> positions, Deque<String> directions) {
+    public static void handleLeftRightMovement(String directionToMove, int row, int column, int[][] mazePosition) {
         boolean done = false;
 
         while(!done && !exitFound) {
             attempts++;
-
-            System.out.println("ATTEMPT #" +attempts);
-            outputMaze(row, column);
 
             // moving left
             if(directionToMove.equals("left")) {
@@ -101,33 +120,25 @@ public class Question10 {
                 }
             }
 
-            // checks to see if column is equal to either max or min bound of the array. This means we found exit
-            if(column <= 0 || column >= mazePosition[0].length) {
-                System.out.println("Exit found!\n");
-                done = true;
+            System.out.println("ATTEMPT #" +attempts);
+            outputMaze(row, column, mazePosition);
+
+            // if the current position has a value of 2, the exit is found
+            if(mazePosition[row][column] == 2) {
+                System.out.println("Exit found in " +attempts+ " attempts!\n");
                 exitFound = true;
             }
 
-            // as we're moving along, check if there are any paths along the way
-            if(mazePosition[row-1][column] == 1) {
-                positions.push(new int[] {row, column});
-                directions.push(W);
-            }
-            if(mazePosition[row+1][column] == 1) {
-                positions.push(new int[] {row, column});
-                directions.push(E);
-            }
+            // as we're moving along, check if there are any paths along the way and only push it if it's not already in vistedIntersections
+            checkPathsAtIntersection(mazePosition, "up&down", row, column);
         }
     }
 
-    public static void handleUpDownMovement(String directionToMove, int row, int column, int[][] mazePosition, Deque<int[]> positions, Deque<String> directions) {
+    public static void handleUpDownMovement(String directionToMove, int row, int column, int[][] mazePosition) {
         boolean done = false;
 
         while(!done && !exitFound) {
             attempts++;
-
-            System.out.println("ATTEMPT #" +attempts);
-            outputMaze(row, column);
 
             // moving up
             if(directionToMove.equals("up")) {
@@ -148,31 +159,56 @@ public class Question10 {
                 }
             }
 
-            // checks to see if column is equal to either max or min bound of the array. This means we found exit
-            if(row <= 0 || row >= mazePosition[0].length) {
-                System.out.println("Exit found!\n");
-                done = true;
+            System.out.println("ATTEMPT #" +attempts);
+            outputMaze(row, column, mazePosition);
+
+            // if the current position has a value of 2, the exit is found
+            if(mazePosition[row][column] == 2) {
+                System.out.println("Exit found in " +attempts+ " attempts!\n");
                 exitFound = true;
             }
 
             // as we're moving along, check if there are any paths along the way
-            if(mazePosition[row][column-1] == 1) {
+            checkPathsAtIntersection(mazePosition, "left&right", row, column);
+        }
+    }
+
+    public static void checkPathsAtIntersection(int[][] mazePosition, String direction, int row, int column) {
+        // this checks for paths below or above the user's position
+        if(direction.equals("up&down")) {
+            if(mazePosition[row-1][column] == 1 && !visitedIntersections.contains(row+ "" +column+ "up")) {
+                // ie. 34right.
+                visitedIntersections.add(row+ "" +column+ "up");
+                positions.push(new int[] {row, column});
+                directions.push(N);
+            }
+            if(mazePosition[row+1][column] == 1 && !visitedIntersections.contains(row+ "" +column+ "down")) {
+                visitedIntersections.add(row+ "" +column+ "down");
+                positions.push(new int[] {row, column});
+                directions.push(S);
+            }
+        }
+        // this checks for paths on the left or right of the user's position
+        else {
+            if(mazePosition[row][column-1] == 1 && !visitedIntersections.contains(row+ "" +column+ "left")) {
+                visitedIntersections.add(row+ "" +column+ "left");
                 positions.push(new int[] {row, column});
                 directions.push(W);
             }
-            if(mazePosition[row][column+1] == 1) {
+            if(mazePosition[row][column+1] == 1 && !visitedIntersections.contains(row+ "" +column+ "right")) {
+                visitedIntersections.add(row+ "" +column+ "right");
                 positions.push(new int[] {row, column});
                 directions.push(E);
             }
         }
     }
 
-    public static int[][] createMaze() {
+    public static int[][] createPDFMaze() {
         return new int[][] {
                 {0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 1, 1, 1, 1, 1, 1, 0},
                 {0, 0, 0, 0, 1, 0, 0, 0},
-                {1, 1, 1, 1, 1, 1, 1, 0},
+                {2, 1, 1, 1, 1, 1, 1, 0},
                 {0, 0, 0, 0, 1, 0, 0, 0},
                 {0, 1, 1, 1, 1, 0, 0, 0},
                 {0, 0, 0, 0, 1, 0, 0, 0},
@@ -180,10 +216,34 @@ public class Question10 {
         };
     }
 
-    // parameters act as a way to keep track where the user is moving
-    public static void outputMaze(int startingRow, int startingColumn) {
-        int[][] maze = createMaze();
+    public static int[][] createMazeWithLoop() {
+        return new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 1, 0, 0, 0, 1, 0},
+                {2, 1, 1, 0, 0, 0, 1, 0},
+                {0, 0, 1, 0, 0, 0, 1, 0},
+                {0, 0, 1, 0, 0, 0, 1, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+    }
 
+    public static int[][] createMazeWithNoExit() {
+        return new int[][] {
+                {0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 1, 0, 1, 0},
+                {0, 1, 0, 1, 0, 1, 1, 0},
+                {0, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 1, 1, 1, 1, 1, 0},
+                {0, 0, 0, 1, 0, 0, 1, 0},
+                {0, 1, 1, 1, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0}
+        };
+    }
+
+    // parameters act as a way to keep track where the user is moving
+    public static void outputMaze(int startingRow, int startingColumn, int[][] maze) {
         for(int i = 0; i < maze.length; i++) {
             if(i != 0)  {
                 System.out.println();
